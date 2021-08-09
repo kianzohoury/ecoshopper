b08a195d92f452e4f1158acb8ded01238dc48b32
Evelyn Lu <evelyn.lu099@gmail.com>
2021-08-08 20:40:25 -0700

    add barcode django api endpoint prototype

--- final/ecoshopper/urls.py
+++ final/ecoshopper/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail')
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('barcode/<int:upc_code>/', views.barcode, name='detail')
 ]
--- final/ecoshopper/views.py
+++ final/ecoshopper/views.py
@@ -1,3 +1,5 @@
+from django.http.request import HttpRequest
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
@@ -13,3 +15,35 @@ def index(request):
 # Replae with another view later. 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
+
+def barcode(request: HttpRequest, upc_code: int):
+    """Given a barcode upc_code, respond the results for the associated object.
+    including object name, recyclable, reusable, Eco Score, alternatives, etc
+
+    Args:
+        request (HttpRequest): request object
+        upc_code (int): the upc code after scanning
+
+    Returns:
+        JsonResponse: the JSON data for the frontend to display.
+    """
+
+    return JsonResponse({
+        'upc': upc_code,
+        'object': 'toothpaste',
+        'recyclable': True,
+        'reusable': False,
+        'eco score': 80,
+        'alternatives': [
+            {
+                'name': 'toothpaste tablets',
+                'imgUrl': 'https://www.byrdie.com/thmb/zru3dagcpw1H-A-UmcVPy9bU_pc=/900x0/filters:no_upscale():max_bytes(150000):strip_icc()/mint-bottlecopy-d697fb68cd0e4d9e862b77cfbc2a2c69.jpg',
+                'productLink': 'https://www.amazon.com/Sustainable-Toothpaste-Tablets-Fluoride-Essentials/dp/B08TTHQF5Q'
+            },
+            {
+                'name': 'metal tube toothpaste',
+                'imgUrl': 'https://m.media-amazon.com/images/I/71IxL-u9XhL._SL1500_.jpg',
+                'productLink': 'https://www.amazon.com/Davids-Natural-Toothpaste-Antiplaque-Fluoride-Free/dp/B00ZTJC26C'
+            }
+        ]
+    })
