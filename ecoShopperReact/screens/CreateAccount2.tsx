22d0f4c988b716d2e56b4c0a9e04a7f0c108c6e5
Evelyn Lu <evelyn.lu099@gmail.com>
2021-08-12 04:44:01 -0700

    slider

--- ecoShopperReact/screens/CreateAccount2.tsx
+++ ecoShopperReact/screens/CreateAccount2.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
-import { StyleSheet, Text, TextInput } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { useTheme } from '@react-navigation/native';
+import { Colors } from 'react-native-paper';
+import MultiSlider from '@ptomasroos/react-native-multi-slider';
+import Slider from '@react-native-community/slider'
 
 import { BlackButton } from '../components/BlackButton';
-import { View } from '../components/Themed';
 
-export default function CreateAccountPage2({ navigation }: { navigation: any }) {
+function CustomSliderMarker() {
+  return <View style={{ height: 40, width: 20 }}>
+    <View style={{
+      height: 40,
+      width: 10,
+      backgroundColor: Colors.orange900
+    }}>
+    </View>
+  </View>
+}
+
+export default function CreateAccountPage2({ navigation, route }: { navigation: any, route: any }) {
   const { colors } = useTheme();
 
   const styles = StyleSheet.create({
@@ -19,7 +32,7 @@ export default function CreateAccountPage2({ navigation }: { navigation: any }) 
       fontSize: 30,
       fontWeight: 'bold',
       textAlign: 'center',
-      marginBottom: 100
+      marginBottom: 40
     },
     sidebar: {
       fontSize: 30,
@@ -31,17 +44,35 @@ export default function CreateAccountPage2({ navigation }: { navigation: any }) 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Describe your current level of recycling. </Text>
-      <Text style={styles.sidebar}>[Slidebar goes here]</Text>
+      {/* <Text style={styles.sidebar}>[Slidebar goes here]</Text> */}
+      <View style={{ width: '100%', alignItems: 'center', paddingHorizontal: 20, marginVertical: 20 }}>
+        <View style={{ flexDirection: 'row', width: 305, paddingRight: 5, justifyContent: 'space-between', marginBottom: -40 }}>
+          {[...Array(9).keys()].map(n => (
+            <View style={{ height: 30, width: 4, backgroundColor: Colors.amber700 }}>
+            </View>))}
+        </View>
+        <MultiSlider
+          sliderLength={300}
+          min={1}
+          max={9}
+          step={1}
+          values={[5]}
+          selectedStyle={{ backgroundColor: Colors.orange900, height: 5, borderLeftColor: Colors.amber900 }}
+          unselectedStyle={{ backgroundColor: Colors.amber700, height: 5 }}
+          customMarker={CustomSliderMarker}
+          snapped={true}
+          allowOverlap={true}
+        />
+        <View style={{ width: 320, flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Text style={{ fontSize: 18, fontWeight: 'bold' }}>never</Text>
+          <Text style={{ fontSize: 18, fontWeight: 'bold' }}>sometimes</Text>
+          <Text style={{ fontSize: 18, fontWeight: 'bold' }}>often</Text>
+        </View>
+      </View>
       <BlackButton onPress={() => {
         console.log('CA2 page next button press');
-        navigation.navigate('CA3');
+        navigation.navigate('CA3', { user: route.param?.user });
       }}>{'Next >'}</BlackButton>
-      <BlackButton onPress={() => {
-        navigation.navigate('NotFoundScreen');
-      }}>{'Not >'}</BlackButton>
-      <BlackButton onPress={() => {
-        navigation.navigate('FoundScreen');
-      }}>{'Found'}</BlackButton>
     </View>
   );
 }
