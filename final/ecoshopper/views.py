21fd89f8634ecfca25d7b6823b0b39a35d5c7a35
Kian Zohoury <kianzohoury@berkeley.edu>
2022-09-09 01:57:45 -0700

    cleaned up python files

--- final/ecoshopper/views.py
+++ final/ecoshopper/views.py
@@ -1,3 +1,10 @@
+import gdown
+import os
+import torch
+import pathlib
+import numpy as np
+
+
 from django.http.request import HttpRequest
 from django.http.response import JsonResponse
 from django.shortcuts import render
@@ -6,33 +13,30 @@ from django.http import HttpResponse
 from .prediction.model import WasteNet
 from .prediction.predict import get_prediction
 
-import gdown
-import os
-import torch
-import pathlib
-import numpy as np
 
-# Download model state
-# url = 'https://drive.google.com/drive/u/0/folders/1kme1P1f_Dcly31rDtXDImtXa9amaYJ9B'
-# make sure to be in final-project directory
-full_path = pathlib.Path.cwd().joinpath('ecoshopper', 'prediction', 'combined_model.pth')
-# gdown.download(url, model_state_path, quiet=False)
+full_path = pathlib.Path.cwd().joinpath(
+    'ecoshopper', 'prediction', 'combined_model.pth'
+)
 
 # Load model state from .pth file
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = WasteNet().to(device)
-model.load_state_dict(torch.load(full_path, map_location=device)['model_state_dict'])
+model.load_state_dict(
+    torch.load(full_path, map_location=device)['model_state_dict']
+)
+
 
 def index(request):
-    # harcoded
+    # Hardcoded for default index page.
     image, title, link, img_link, class_label = get_prediction(
-                                                    model,
-                                                    "coca cola",
-                                                    num_results=5,
-                                                    size=(300, 300),
-                                                    view=""
-                                                    )
+        model,
+        "coca cola",
+        num_results=5,
+        size=(300, 300),
+        view=False
+    )
     return HttpResponse(class_label)
+
 
 def barcode(request: HttpRequest, upc_code: int):
     """Given a barcode upc_code, respond the results for the associated object.
@@ -46,12 +50,12 @@ def barcode(request: HttpRequest, upc_code: int):
         JsonResponse: the JSON data for the frontend to display.
     """
     image, title, link, img_link, class_label = get_prediction(
-                                                    model,
-                                                    upc_code,
-                                                    num_results=5,
-                                                    size=(300, 300),
-                                                    view=""
-                                                    )
+        model,
+        upc_code,
+        num_results=5,
+        size=(300, 300),
+        view=False
+    )
     return JsonResponse({
         'upc': upc_code,
         'object': title,
@@ -62,13 +66,16 @@ def barcode(request: HttpRequest, upc_code: int):
         'alternatives': get_alternatives(upc_code)
     })
 
+
 def get_alternatives(upc_code):
     # alternative_dict = {
     #
     # }
     return ""
 
+
 def heuristic(title, recyclable):
+    """Simple recycling score heuristic."""
     score = 0
     if recyclable == "RECYCLABLE":
         score += 2
